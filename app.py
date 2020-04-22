5d3af8de85924083b125c6f2b350e48eaf09cf04
Oscar Peñaloza <oscar.penalozao@utem.cl>
2020-04-22 05:29:02 -0400

    Update app.py
    Ahora con funciones para grafo dirigido y no dirigido.

--- app.py
+++ app.py
@@ -5,47 +5,45 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-app= Flask(__name__,static_url_path='/static')
-
+app= Flask(__name__)
 app.config['SECRET_KEY'] = '64fa09145bd39641bbc316d33c351458'   #Proteccion contra ataques por cookies,etc.
 #token HEX(16) generado con la libreria secrets
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 @app.route("/")
 def pload():
-     return render_template("pre-load.html")
-     
-
-
-
-
-
+    return render_template("pre-load.html")
 
 @app.route("/home",methods=['GET','POST'])
 def home():
-    
     form = NodesClass()
     if form.validate_on_submit():
-        # pasa el dato de node a grafos.
-        return redirect(url_for("plot",form = str(form.node.data))) 
+        if form.check.data == True:
+            info = "1"+"&"+form.edge.data
+        else:
+            info = "0"+"&"+form.edge.data
+             #concadena los datos para enviarlos por url
+        return redirect(url_for("plot",form = info))
     return render_template("index.html", title='Home',form = form)
 
-@app.route("/grafo<string:form>")
-def plot(form):
-
-    g = nx.DiGraph()
-
-    g.clear()
-    plt.clf()
-
-    a = []
-    s = form.split(";")
-    for i in s:
-        b = i.split(",")
-        a.append(b)
+@app.route("/grafo/<string:form>")
+def plot(form):#ejemplo: 1&1,2;2,3;3,4;2,4;5,5;6,2 o 0&1,2;2,3;3,4;2,4;5,5;6,2
+    separar_informacion = form.split("&")
+    edges0 = separar_informacion[1].split(";")
+    edges = []
+    for i in edges0:
+        edges.append(tuple(i.replace(',',''))) #agrega los vertices en tuplas tipo [('1', '2'), ('2', '3'), ('3', '4'), ('2', '4'), ('5', '5'), ('6', '2')]
     
-    g.add_edges_from(a)
-    nx.draw(g)
+    if separar_informacion[0] == "1":#Crea o no un grafo dirigido.
+        g = nx.DiGraph()
+    else:
+        g = nx.Graph()
+    
+    g.add_edges_from(edges)
+    nx.draw(g,with_labels=True)
     plt.savefig('static/images/graph.png')
 
+    plt.clf()#limpia los parametros
+    g.clear()
     return render_template("grafo.html",url='/static/images/graph.png')
 
 
